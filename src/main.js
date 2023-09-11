ea30d709530f5fea88d27a96132167785c84c661
99ru <yan.99ru@gmail.com>
2023-09-11 16:51:01 +0200

    canva drawing

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueKonva from 'vue-konva'
 
-const app = createApp(App)
 
-// Use VueKonva plugin
-app.use(VueKonva)
-
-app.mount('#app')
+createApp(App).mount('#app')
